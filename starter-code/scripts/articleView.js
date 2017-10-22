4eeb0cc49f647da34555f866932eba566ac59145
Ivan Limongan <ivan.limongan@gmail.com>
2017-10-22 14:38:49 -0700

    filter for category works

--- starter-code/scripts/articleView.js
+++ starter-code/scripts/articleView.js
@@ -59,6 +59,20 @@ articleView.handleCategoryFilter = function() {
   //       When an option with a value is selected, hide all the articles, then reveal the matches.
   //       When the blank (default) option is selected, show all the articles, except for the template.
   //       Be sure to reset the #author-filter while you are at it!
+  $('#category-filter').on('change', function() {
+    if ($(this).val()) {
+    
+      $('article').hide();
+      console.log($(this).val());
+      $('article[data-category="' +$(this).val()+ '"]').fadeIn(1000);
+
+    } else {
+      
+      $('article').show();
+      $('.template').hide();
+    }
+    $('#author-filter').val('');
+  });
 
 };
 
@@ -90,7 +104,7 @@ articleView.setTeasers = function() {
 $(document).ready(function() {
 articleView.populateFilters();
 articleView.handleAuthorFilter();
-// articleView.handleCategoryFilter();
+articleView.handleCategoryFilter();
 // articleView.handleMainNav();
 // articleView.setTeasers();
 })
