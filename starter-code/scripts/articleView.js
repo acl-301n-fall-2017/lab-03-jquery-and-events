36731f7b353b369a8744f3a90609816fc7c49f24
Mario Quintana <pyrros2000@gmail.com>
2018-05-01 18:12:38 -0700

    added script tag, placed functions in document ready function

--- starter-code/scripts/articleView.js
+++ starter-code/scripts/articleView.js
@@ -82,7 +82,11 @@ articleView.setTeasers = function() {
 
 };
 
-// TODO: Call all of the above functions, once we are sure the DOM is ready.
+// TODONE: Call all of the above functions, once we are sure the DOM is ready.
 $(document).ready(function() {
-
+  articleView.populateFilters();
+  articleView.handleAuthorFilter();
+  articleView.handleCategoryFilter();
+  articleView.handleMainNav();
+  articleView.setTeasers();
 })
